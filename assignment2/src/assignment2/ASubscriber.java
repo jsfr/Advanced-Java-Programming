450692701a43ea27dcfc6ba23e676149a8a74532
jsfr <jfrdev@gmail.com>
2013-11-12 14:20:40 +0100

    ASubscriber implemented

--- assignment2/src/assignment2/ASubscriber.java
+++ assignment2/src/assignment2/ASubscriber.java
@@ -1,31 +1,67 @@
 package assignment2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ASubscriber implements Subscriber, Runnable {
 
-	public void run() {
-		while (true) {
-			int discomfortLevel = this.getDiscomfortWarning();
-			this.processDiscomfortWarning(discomfortLevel);
-		}
-	}
+    Queue<Integer> readings = new LinkedList<Integer>();
 
-	@Override
-	public void pushDiscomfortWarning(int discomfortlevel) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void run() {
+        while (true) {
+            int discomfortLevel = this.getDiscomfortWarning();
+            this.processDiscomfortWarning(discomfortLevel);
+        }
+    }
 
-	@Override
-	public void processDiscomfortWarning(int discomfortLevel) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void pushDiscomfortWarning(int discomfortlevel) {
+        synchronized(this) {
+            if (readings.size() < 50) {
+                readings.add(Integer.valueOf(discomfortlevel));
+                notify();
+            }
+        }
+    }
 
-	@Override
-	public int getDiscomfortWarning() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public void processDiscomfortWarning(int discomfortLevel) {
+        switch (discomfortLevel) {
+        case 1:
+            System.out.println("Feeling so nice (^_-)");
+            break;
+        case 2:
+            System.out.println("Still quite okay.");
+            break;
+        case 3:
+            System.out.println("This is not nice.");
+            break;
+        case 4:
+            System.out.println("It is uncomfortable.");
+            break;
+        case 5:
+            System.out.println("It hate this planet!");
+            break;
+        case 0:
+            System.out.println("getDiscomfortWarning was interrupted while waiting.");
+        default:
+            System.out.println("Something weird and undefined happen with the weather (and the program).");
+        }
+    }
+
+    @Override
+    public int getDiscomfortWarning() {
+        synchronized(this) {
+            while (readings.peek() == null) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    return 0;
+                }
+            }
+            return readings.poll().intValue();
+        }
+    }
 
 
 
