7ea1c3bc092ae1b9276611f52398aa5f5aaa7437
Henrik <henrik.bendt@gmail.com>
2013-11-12 14:35:16 +0100

    Asensor donejohn

--- assignment2/src/assignment2/ASensor.java
+++ assignment2/src/assignment2/ASensor.java
@@ -1,30 +1,44 @@
 package assignment2;
 
 import java.util.List;
+import java.util.Random;
 
 public class ASensor implements Sensor, Runnable {
 	List<Monitor> monitors; //A sensor can push readings to one or many monitors
 	
 	public SensorReading generateSensorReading() {
-		// TODO Auto-generated method stub
-		return null;
+		SensorReading sr = new SensorReading();
+		Random rnd = new Random();
+		Float temp = (float) rnd.nextGaussian()*50+10; //Gausian normal distribution between 10 and 60
+		Float hum = (float) rnd.nextGaussian()*50+50;
+		sr.setHumidity(hum);
+		sr.setTemperature(temp);
+		return sr;
 	}
 
 	public void run() {
 		SensorReading reading = null;
 		while(true) {
 			reading = this.generateSensorReading();
-			for (Monitor sm : monitors) {
-				sm.pushReading(reading);
+			synchronized(this){
+				for (Monitor sm : monitors) {
+					sm.pushReading(reading);
+				}
 			}
-			//Thread.sleep() ??
+			try {
+				Thread.sleep(2000); //sleep for 2 sec.
+			} catch (InterruptedException e) {
+				return; //just die
+			}
 		}
 		
 	}
 
 	@Override
 	public int registerMonitor(List<Monitor> sm) {
-		// TODO Auto-generated method stub
+		synchronized(this){
+			this.monitors.addAll(sm);
+		}
 		return 0;
 	}
 
