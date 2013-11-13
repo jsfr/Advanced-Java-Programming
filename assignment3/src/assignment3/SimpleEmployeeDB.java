1041d7656fab3ff4ee909455521d7683a0dbae5f
Ronni Elken Lindsgaard <rel@it-minds.dk>
2013-11-13 16:22:57 +0100

    Implemented the server db

--- assignment3/src/assignment3/SimpleEmployeeDB.java
+++ assignment3/src/assignment3/SimpleEmployeeDB.java
@@ -1,9 +1,16 @@
 package assignment3;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SimpleEmployeeDB implements EmployeeDB {
 	private static SimpleEmployeeDB instance = null;
+	
+	//Map<Integer,List<Employee>> db = new HashMap<Integer, List<Employee>>();
+	//List<Integer> departments = new ArrayList<Integer>();
+	protected List<Employee> emps = new ArrayList<Employee>();
 
 	private SimpleEmployeeDB() {
 
@@ -18,20 +25,31 @@ public class SimpleEmployeeDB implements EmployeeDB {
 
 	@Override
 	public synchronized void addEmployee(Employee emp) {
-		// TODO Auto-generated method stub
+		//db.put(emp.getDepartment(), emp);
+		emps.add(emp);
 	}
 
 	@Override
 	public synchronized List<Employee> listAllEmployees() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return emps;
 	}
 
 	@Override
 	public synchronized List<Employee> listEmployeesInDept(
 			List<Integer> departmentIds) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Set<Employee> emps = new HashSet<Employee>();
+		
+		for(Employee emp : this.emps) {
+			for(Integer i : departmentIds) {
+				if(emp.getDepartment() == i) {
+					emps.add(emp);	
+				}
+			}		
+		}
+		
+		return new ArrayList<Employee>(emps);
 	}
 
 	@Override
@@ -39,7 +57,38 @@ public class SimpleEmployeeDB implements EmployeeDB {
 			List<SalaryIncrement> salaryIncrements)
 			throws DepartmentNotFoundException,
 			NegativeSalaryIncrementException {
-		// TODO Auto-generated method stub
+		
+		
+		
+		Set<Integer> deps = new HashSet<Integer>();
+		
+		//Check that there is no negative salary
+		for(SalaryIncrement si : salaryIncrements) {
+			if(si.getIncrementBy() < 0) {
+				throw new NegativeSalaryIncrementException();
+			} else {
+				deps.add(si.getDepartment());
+			}
+		}
+		
+		List<Employee> emps = listEmployeesInDept(new ArrayList<Integer>(deps));
+		
+		//Check that all departments exist
+		for(Employee emp : emps) {
+			deps.remove(emp.getDepartment());
+		}
+		if(deps.size() > 0) {
+			throw new DepartmentNotFoundException();
+		}
+		
+		//Do the actual salary increment
+		for(Employee emp : emps) {
+			for(SalaryIncrement si : salaryIncrements) {
+				if(emp.getDepartment() == si.getDepartment()) {
+					emp.setSalary(emp.getSalary() + si.getIncrementBy());
+				}
+			}
+		}
 	}
 
 }
